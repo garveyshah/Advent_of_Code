13c60658f29dac6ce7c6d98b644e460844eba697
oumouma <garveyshah1595@gmail.com>
2024-08-27 06:06:53 +0300

    add code for calling GetPosition

--- day_1/main.go
+++ day_1/main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"1/brackets"
 	"1/utility"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 && len(os.Args) != 3 {
 		fmt.Println("Usage: go run . <FileName>\ngo run . data.txt")
 		return
 	}
 
-	FileName := os.Args[1]
+	var FileName string
+	var floor uint
+
+	if len(os.Args) == 2 {
+		FileName = os.Args[1]
+	} else {
+		FileName = os.Args[1]
+		flo, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		floor = uint(flo)
+	}
+
 	content, err := utility.GetData(FileName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(brackets.SortBrackets(string(content)))
+	//fmt.Println(brackets.SortBrackets(string(content)))
+	fmt.Println(brackets.GetPosition(content,floor))
 }
-
