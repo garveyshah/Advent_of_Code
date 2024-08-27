dfd8ec3f5351c36a7d14ed236359ccd779078fac
oumouma <garveyshah1595@gmail.com>
2024-08-27 06:30:46 +0300

    initial code for pakg bracket tests

---
+++ day_1/brackets/brackets_test.go
@@ -0,0 +1,16 @@
+package brackets
+
+import "testing"
+
+func TestSortBrackets(t *testing.T) {
+	tt := []struct {
+		name  string
+		input string
+		want  uint
+	}{
+		// test case
+	}
+	for _, tc := range tt {
+		r.Run()
+	}
+}
