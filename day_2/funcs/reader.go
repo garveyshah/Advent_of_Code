71e38dcc7e7e12518f36580ab25709c2135c6fad
oumouma <garveyshah1595@gmail.com>
2024-08-28 12:35:10 +0300

    feat: Reader() function, to open and read the file

--- day_2/funcs/reader.go
+++ day_2/funcs/reader.go
@@ -1,12 +1,28 @@
 package funcs
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func Reader(FileName string) []int {
-	return []int{}
+func Reader(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return []string{}, fmt.Errorf("error opening file %v\n: %v", filename, err)
+	}
+	defer file.Close()
+
+	fileScan := bufio.NewScanner(file)
+	fileScan.Split(bufio.ScanLines)
+	data := []string{}
+
+	for fileScan.Scan() {
+		data = append(data, fileScan.Text())
+	}
+	return data, nil
 }
 
 func DecodeData(s []string) [][]int {
