44859a8684dba2e8163d500e48ad6f310640c815
oumouma <garveyshah1595@gmail.com>
2024-08-28 12:11:13 +0300

    feat: DecodeData(), to format the data from the file to a more workable format

--- day_2/funcs/reader.go
+++ day_2/funcs/reader.go
@@ -1,5 +1,29 @@
 package funcs
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Reader(FileName string) []int {
 	return []int{}
+}
+
+func DecodeData(s []string) [][]int {
+	var Jug [][]int
+	var cup []int
+	for _, str := range s {
+		Nstr := strings.Split(str, "x")
+
+		for _, char := range Nstr {
+			num, err := strconv.Atoi(char)
+			if err != nil {
+				continue
+			}
+			cup = append(cup, num)
+		}
+		Jug = append(Jug, cup)
+		cup = []int{}
+	}
+	return Jug
 }
