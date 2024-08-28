049827bd113f3321c286e39f5641dd9e9b114773
oumouma <garveyshah1595@gmail.com>
2024-08-28 12:42:44 +0300

    Refactore: main() function to calling functions only

--- day_2/main.go
+++ day_2/main.go
@@ -1,24 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"2/funcs"
+)
 
 func main() {
-	l, w, h := 2, 3, 4
-	l1, w2, h3 := 1, 1, 10
-
-	TFeet := SurfaceArea(l, w, h)
-	TFeet2 := SurfaceArea(l1, w2, h3)
-	fmt.Println("instance A:= ", TFeet)
-	fmt.Println("Instance B := ", TFeet2)
-}
-
-func SurfaceArea(l, w, h int) int {
-	sa := (2*l*w + 2*h*w + 2*h*l)
-
-	if (l == w) || (l == h) || (w == h) {
-		sa += 1
-	} else {
-		sa += 6
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . <filePath>\nexample: go run . data.txt")
+		return
 	}
-	return sa
+
+	filename := os.Args[1]
+
+	data, err := funcs.Reader(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	Jug := funcs.DecodeData(data)
+	fmt.Println(Jug)
+	totalSa := funcs.TotalSa(Jug)
+	fmt.Println("Total SA ==", totalSa)
+	// l, w, h := 2, 3, 4
+	// l1, w2, h3 := 1, 1, 10
+
+	// TFeet := funcs.SurfaceArea(l, w, h)
+	// TFeet2 := funcs.SurfaceArea(l1, w2, h3)
+	// fmt.Println("instance A:= ", TFeet)
+	// fmt.Println("Instance B := ", TFeet2)
 }
