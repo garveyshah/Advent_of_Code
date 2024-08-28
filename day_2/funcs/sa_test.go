1196dc9ff27baaca96e8ae8c3340b55e31e21da7
oumouma <garveyshah1595@gmail.com>
2024-08-28 22:06:10 +0300

    test

---
+++ day_2/funcs/sa_test.go
@@ -0,0 +1,64 @@
+package funcs
+
+import "testing"
+
+func TestWrappPaper(t *testing.T) {
+	tt := []struct {
+		name  string
+		input []int
+		want  int
+	}{
+		{"Test 1", []int{2, 3, 4}, 58},
+		{"Test 2", []int{1, 1, 10}, 43},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := WrappPaper(tc.input)
+			if got != tc.want {
+				t.Errorf("%q Failed: got %q, want %q", tc.name, got, tc.want)
+			}
+		})
+	}
+}
+
+func TestTotalSa(t *testing.T) {
+	tt := []struct {
+		name  string
+		input [][]int
+		want  int
+	}{
+		{"Test 1", [][]int{{2, 3, 4}, {1, 1, 10}}, 101},
+		{"Test 2", [][]int{{2, 3, 4}, {1, 1, 10}}, 101},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := TotalSa(tc.input)
+			if got != tc.want {
+				t.Errorf("%q Failed: got %q, want %q", tc.name, got, tc.want)
+			}
+		})
+	}
+}
+func TestMin(t *testing.T) {
+	tt := []struct {
+		name  string
+		a int
+		b int
+		c int
+		want  int
+	}{
+		{"Test 1", 2, 3, 4, 2},
+		{"Test 2", 1, 1, 10, 1},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := Min(tc.a, tc.b, tc.c)
+			if got != tc.want {
+				t.Errorf("%q Failed: got %v, want %v", tc.name, got, tc.want)
+			}
+		})
+	}
+}
