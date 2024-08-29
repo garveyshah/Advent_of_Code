2f3240a6065a1de3d837d3038b651aa3e4e69f6d
oumouma <garveyshah1595@gmail.com>
2024-08-29 18:45:47 +0300

    feat: error handling: data retrival from the terminal

--- day_3/main.go
+++ day_3/main.go
@@ -1,6 +1,21 @@
 package main
 
-//
-func houses(str string) int {
-	
+import (
+	"fmt"
+	"os"
+
+	"day3/funcs"
+	"day3/utils"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage : go run . <filename>\nExample: go run . data.txt")
+		return
+	}
+	data, err:= utils.Reader(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println("Houses gifted are : ", funcs.HouseTracker(data))
 }
