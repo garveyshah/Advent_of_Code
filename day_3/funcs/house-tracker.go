1ee654e7b4a9959777e29e3eaf1abd7a7c6f949d
oumouma <garveyshah1595@gmail.com>
2024-08-31 04:12:27 +0300

    feat: part two solution

--- day_3/funcs/house-tracker.go
+++ day_3/funcs/house-tracker.go
@@ -27,50 +27,41 @@ func HouseTracker(s string) int {
 }
 
 func HouseTracker2(s string) int {
-		HouseMap := make(map[[2]int]bool)
-		HouseMap1 := make(map[[2]int]bool)
+	HouseMap := make(map[[2]int]bool)
+	var count int
 
-	var count,count1 int
-	House := [2]int{0, 0}
+	// Starting positions for Santa and the robot
+	santa := [2]int{0, 0}
+	robot := [2]int{0, 0}
 
-	HouseMap[House] = true
+	// Mark the inital position as visited
+	HouseMap[santa] = true
 	count++
-	HouseMap1[House] = true
-	count1++
 
 	for i, seen := range s {
-
+		// Determine whether it;s santa's or robot's turn to
+		var current *[2]int
 		if i%2 == 0 {
-			if seen == '>' {
-				House[0] += 1
-			} else if seen == '^' {
-				House[1] += 1
-			} else if seen == 'v' {
-				House[1] -= 1
-			} else if seen == '<' {
-				House[0] -= 1
-			}
-			if !HouseMap1[House] {
-				HouseMap1[House] = true
-				count1++
-			}
-			//count1 -= 1
+			current = &santa
 		} else {
-			if seen == '>' {
-				House[0] += 1
-			} else if seen == '^' {
-				House[1] += 1
-			} else if seen == 'v' {
-				House[1] -= 1
-			} else if seen == '<' {
-				House[0] -= 1
-			}
-			if !HouseMap[House] {
-				HouseMap[House] = true
-				count++
-			}
+			current = &robot
+		}
+
+		if seen == '>' {
+			current[0] += 1
+		} else if seen == '^' {
+			current[1] += 1
+		} else if seen == 'v' {
+			current[1] -= 1
+		} else if seen == '<' {
+			current[0] -= 1
+		}
+
+		// Check if this position has been visited before
+		if !HouseMap[*current] {
+			HouseMap[*current] = true
+			count++
 		}
 	}
-	tcount := count + count1
-	return tcount
+	return count
 }
