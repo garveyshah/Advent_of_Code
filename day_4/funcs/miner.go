fc4dd6f378a9d5131579d4b05a88c201141b0370
oumouma <garveyshah1595@gmail.com>
2024-09-02 06:42:36 +0300

    feat: StringBuilder(), CustomAtoi(), custom helper functions 1. to build strings 2. To covert int string

--- day_4/funcs/miner.go
+++ day_4/funcs/miner.go
@@ -4,4 +4,23 @@ func MD5(s string) string {
 	return "Godwin"
 }
 
-//
+// StringBuilder to concatenate secret key and number
+func StringBuilder(sKey string, num int) string {
+	return sKey + CustomAtoi(num)
+}
+
+// CustomAtoi to convert interger to string
+func CustomAtoi(num int) string {
+	var result string
+
+	for num > 0 {
+		result = string('0'+(num%10)) + result
+		num /= 10
+	}
+
+	if result == "" {
+		return "0"
+	}
+
+	return result
+}
