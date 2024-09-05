354f45bbe2362d22d69b2d74c09eee45080c2905
oumouma <garveyshah1595@gmail.com>
2024-09-06 00:29:14 +0300

    function calling and error handling'

--- day_5/main.go
+++ day_5/main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"day5/funcs"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage : go run . <fileName>\nExample : go run . data.txt")
+		return
 	}
 
-	result := funcs.StringSort(os.Args[1])
-	fmt.Println(result)
+	data, err := funcs.Reader(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+
+	Nice, Naughty := funcs.Counter(data)
+
+	fmt.Println("Nice - ", Nice)
+	fmt.Println("Naughty - ", Naughty)
 }
