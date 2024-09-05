f9fdd37f18abe08879c1247216cbbf9d698d798f
oumouma <garveyshah1595@gmail.com>
2024-09-06 00:25:56 +0300

    feat: functions that implement reading, validation and counting of nice strings

--- day_5/funcs/funcs_test.go
+++ day_5/funcs/funcs_test.go
@@ -16,11 +16,11 @@ func TestNaughtyOrNice(t *testing.T) {
 		{"Test 3", "dvszwmarrgswjxmb", "naughty"},
 		{"Test 4", "ugknbfddgicrmopn", "nice"},
 		{"Test 5", "aaa", "nice"},
-		{"Test 6", "xazegov", "nice"},
-		{"Test 7", "aei", "nice"},
-		{"Test 8", "aeiouaeiouaeiou", "nice"},
-		{"Test 9", "aabbccdd", "nice"},
-		{"Test 9", "abcdde", "nice"},
+		{"Test 6", "xazegov", "naughty"},
+		{"Test 7", "aei", "naughty"},
+		{"Test 8", "aeiouaeiouaeiou", "naughty"},
+		{"Test 9", "aabbccdd", "naughty"},
+		{"Test 10", "abcdde", "naughty"},
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
@@ -31,3 +31,26 @@ func TestNaughtyOrNice(t *testing.T) {
 		})
 	}
 }
+
+func TestDoubleLetters(t *testing.T) {
+	tt := []struct {
+		name  string
+		input string
+		want  bool
+	}{
+		{"Test 1", "aabbccdd", true},
+		{"Test 2", "abcdde", true},
+		{"Test 3", "xazegov", false},
+		{"Test 4", "aeiouaeiouaeiou", false},
+		{"Test 5", "ugknbfddgicrmopn", true},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := DoubleLetters(tc.input)
+			if got != tc.want {
+				t.Errorf("%q Failed : got %v, want %v", tc.name, got, tc.want)
+			}
+		})
+	}
+}
+
