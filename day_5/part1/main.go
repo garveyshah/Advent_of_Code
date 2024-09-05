634a4fee4e3a6930b0bb713a0951b961baf84507
oumouma <garveyshah1595@gmail.com>
2024-09-06 02:28:05 +0300

    part1 solution of day5 advent of code 2015

---
+++ day_5/part1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"day5/part1/funcs"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage : go run . <fileName>\nExample : go run . data.txt")
+		return
+	}
+
+	data, err := funcs.Reader(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	Nice, Naughty := funcs.Counter(data)
+
+	fmt.Println("Nice - ", Nice)
+	fmt.Println("Naughty - ", Naughty)
+}
