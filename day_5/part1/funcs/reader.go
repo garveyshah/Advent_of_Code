68cbbfb294f496efa1a2992a9fe277a8594ee60d
oumouma <garveyshah1595@gmail.com>
2024-09-18 00:17:11 +0300

    day_5 final implementation

--- day_5/part1/funcs/reader.go
+++ day_5/part1/funcs/reader.go
@@ -1,24 +1,38 @@
 package funcs
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Reader(filename string) ([]string, error) {
-	file, err := os.Open(filename)
+	// file, err := os.Open(filename)
+	// if err != nil {
+	// 	return []string{}, fmt.Errorf("error opening file %v\n: %v", filename, err)
+	// }
+	// defer file.Close()
+
+	// fileScan := bufio.NewScanner(file)
+	// fileScan.Split(bufio.ScanLines)
+	// data := []string{}
+
+	// for fileScan.Scan() {
+	// 	data = append(data, fileScan.Text())
+	// }
+	// return data, nil
+
+	file, err := os.ReadFile(filename)
 	if err != nil {
-		return []string{}, fmt.Errorf("error opening file %v\n: %v", filename, err)
+		return nil, fmt.Errorf("error reading file %q: %v ", filename, err)
 	}
-	defer file.Close()
 
-	fileScan := bufio.NewScanner(file)
-	fileScan.Split(bufio.ScanLines)
-	data := []string{}
+	if len(file) < 1 {
+		return nil, fmt.Errorf("file is empty %q", filename)
 
-	for fileScan.Scan() {
-		data = append(data, fileScan.Text())
 	}
-	return data, nil
+
+	str := string(file)
+	words := strings.Fields(str)
+	return words, nil
 }
--- day_5/part2/functions/functions.go
+++ day_5/part2/functions/functions.go
@@ -1,18 +1,10 @@
 package functions
 
-func overlapChecker(s string) bool {
-	for i := 0; i < len(s)-2; i++ {
-		if s[i] == s[i+1] && s[i+1] == s[i+2] {
-			return false
-		}
-	}
-	return true
-}
-
 func pairCounter(s string) bool {
 	pairMap := make(map[string]int)
 
 	for i := 0; i < len(s)-1; i++ {
+		
 		// Form the current pair
 		pair := string(s[i]) + string(s[i+1])
 
@@ -21,14 +13,20 @@ func pairCounter(s string) bool {
 
 		// if a pair appears more than once, return true
 		if pairMap[pair] > 1 {
-			return true
+
+			// Ensure non-overlapping pairs by checking the distance
+			for j := 0; j < i-1; j++ {
+				if s[j:j+2] == pair {
+					return true
+				}
+			}
 		}
 	}
 	return false
 }
 
 func IsNice(s string) bool {
-	return  pairCounter(s) && overlapChecker(s) && intervaledPair(s)
+	return pairCounter(s) &&  intervaledPair(s)
 }
 
 func intervaledPair(s string) bool {
@@ -39,4 +37,3 @@ func intervaledPair(s string) bool {
 	}
 	return false
 }
-
